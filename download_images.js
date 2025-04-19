4d0f2fd2228974279dc6344d6c31ecd26ea89629
amine.lamchatab <115371227+LamchatabAmine@users.noreply.github.com>
2025-04-19 12:55:37 +0100

    save

--- download_images.js
+++ download_images.js
@@ -4,38 +4,31 @@ const https = require('https');
 const http = require('http');
 
 const imageUrls = [
-  "https://uprecipes.blog/wp-content/uploads/2025/04/cinnamon-donut-bread-every-bite-of.jpg",
-  "https://uprecipes.blog/wp-content/uploads/2025/04/mushroom-beef-casserole-ingredients-1-1.jpg",
-  "https://uprecipes.blog/wp-content/uploads/2025/04/double-crust-cheesecake-ingredients-1.jpg",
-  "https://uprecipes.blog/wp-content/uploads/2025/04/low-carb-baked-chicken-tenders-2.jpg",
-  "https://uprecipes.blog/wp-content/uploads/2025/04/crock-pot-potato-broccoli-cheddar-soup.jpg",
-  "https://uprecipes.blog/wp-content/uploads/2025/04/cheesy-chicken-spinach-wrap-ingredients-2.jpg",
-  "https://uprecipes.blog/wp-content/uploads/2025/04/crab-cake-egg-rolls-ingredients-.jpg",
-  "https://uprecipes.blog/wp-content/uploads/2025/04/hashbrown-sausage-bites-ingredients-4-cups.jpg",
-  "https://uprecipes.blog/wp-content/uploads/2025/04/banana-split-icebox-cake-ingredients-for.jpg",
-  "https://uprecipes.blog/wp-content/uploads/2025/04/baltimore-crab-cakes-recipe-ingredients-1.jpg",
-  "https://uprecipes.blog/wp-content/uploads/2025/04/oven-baked-candied-strawberries-ingredients-1.jpg",
-  "https://uprecipes.blog/wp-content/uploads/2025/04/creamy-alfredo-lasagna-soup-ingredients-1.jpg",
-  "https://uprecipes.blog/wp-content/uploads/2025/04/broccoli-rice-cheese-and-chicken-casserole.jpg",
-  "https://uprecipes.blog/wp-content/uploads/2025/04/rice-krispie-cheesecake-ingredients-for-the-1.jpg",
-  "https://uprecipes.blog/wp-content/uploads/2025/04/cranberry-orange-bread-this-fresh-zesty.jpg",
-  "https://uprecipes.blog/wp-content/uploads/2025/04/black-forest-roll-cake-a-chocolate-cherry-delight.jpg",
-  "https://uprecipes.blog/wp-content/uploads/2025/04/moist-coffee-cake-ingredients-cake-6.jpg",
-  "https://uprecipes.blog/wp-content/uploads/2025/04/would-you-eat-sweet-and-savory-honey-roasted-butternut-squash-stuffed-with-brussels-sprouts-beets-carrots-cranberries-and-feta.jpg",
-  "https://uprecipes.blog/wp-content/uploads/2025/04/would-you-eat-black-velvet-cake.jpg",
-  "https://uprecipes.blog/wp-content/uploads/2025/04/cajun-salmon-and-shrimp-ingredients-4.jpg",
-  "https://uprecipes.blog/wp-content/uploads/2025/04/skewers-surf-turf-meets-chimichurri.jpg",
-  "https://uprecipes.blog/wp-content/uploads/2025/04/lobster-crab-and-salmon-alfredo-ingredients.jpg",
-  "https://uprecipes.blog/wp-content/uploads/2025/04/would-you-eat-this-creamy-garlic.jpg",
-  "https://uprecipes.blog/wp-content/uploads/2025/04/would-you-eat-this-cajun-shrimp.jpg",
-  "https://uprecipes.blog/wp-content/uploads/2025/04/mini-strawberry-cheesecakes-indulge-in-these.jpg",
-  "https://uprecipes.blog/wp-content/uploads/2025/04/no-bake-strawberry-cheesecake-lasagna-this.jpg",
-  "https://uprecipes.blog/wp-content/uploads/2025/04/lemon-raspberry-swirl-cheesecake-indulge-in.jpg"
+  "https://uprecipes.blog/wp-content/uploads/2025/04/classic-cherry-delight-ingredients-for-the.jpg",
+  "https://uprecipes.blog/wp-content/uploads/2025/04/homemade-whipped-cinnamon-pumpkin-honey-butter.jpg",
+  "https://uprecipes.blog/wp-content/uploads/2025/04/bang-bang-chicken-skewers-recipe-ingredients.jpg",
+  "https://uprecipes.blog/wp-content/uploads/2025/04/southern-tea-cake-cookies-ingredients-1-1.jpg",
+  "https://uprecipes.blog/wp-content/uploads/2025/04/southern-tea-cake-cookies-ingredients-.jpg",
+  "https://uprecipes.blog/wp-content/uploads/2025/04/no-bake-candy-cane-pie-indulge-in.jpg",
+  "https://uprecipes.blog/wp-content/uploads/2025/04/creamy-garlic-butter-salmon-and-shrimp-768x635.jpg",
+  "https://uprecipes.blog/wp-content/uploads/2025/04/strawberry-chocolate-layer-cake-ingredients-chocolate-768x635.jpg",
+  "https://uprecipes.blog/wp-content/uploads/2025/04/decadent-chocolate-caramel-ice-cream-cake-768x635.jpg",
+  "https://uprecipes.blog/wp-content/uploads/2025/04/surf-and-turf-with-saffron-rice-768x635.jpg",
+  "https://uprecipes.blog/wp-content/uploads/2025/04/red-velvet-cheesecake-recipe-ingredients-for-768x635.jpg",
+  "https://uprecipes.blog/wp-content/uploads/2025/04/creamy-garlic-mushroom-sauce-ingredients-2-768x635.jpg",
+  "https://uprecipes.blog/wp-content/uploads/2025/04/strawberry-pretzel-salad-ingredients-2-cups.jpg",
+  "https://uprecipes.blog/wp-content/uploads/2025/04/surf-and-turf-with-creamy-alfredo-768x635.jpg",
+  "https://uprecipes.blog/wp-content/uploads/2025/04/rice-krispie-cheesecake-ingredients-for-the.jpg",
+  "https://uprecipes.blog/wp-content/uploads/2025/04/garlic-butter-chicken-pasta-ingredients-300g.jpg",
+  "https://uprecipes.blog/wp-content/uploads/2025/04/fudgy-chocolate-brownies-with-strawberries-recipe.jpg",
+  "https://uprecipes.blog/wp-content/uploads/2025/04/mini-banoffee-pies-recipe-ingredients-for.jpg",
+  "https://uprecipes.blog/wp-content/uploads/2025/04/chocolate-raspberry-granola-parfaits-recipe-ingredients.jpg",
+  "https://uprecipes.blog/wp-content/uploads/2025/04/red-velvet-cake-with-whipped-cream.jpg"
 ];
 
 
 
-const saveDirectory = 'C:\\uprecipes\\posts\\17-04-25';
+const saveDirectory = 'C:\\uprecipes\\posts\\19-04-25';
 
 if (!fs.existsSync(saveDirectory)) {
   fs.mkdirSync(saveDirectory, { recursive: true });
